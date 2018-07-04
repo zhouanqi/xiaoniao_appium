50c4971a963cd2ba79fe069bed46c71b420a8b28
simple1_6 <454340056@qq.com>
2018-07-04 18:45:13 +0800

    封装登录方法

--- bussinessView/loginView.py
+++ bussinessView/loginView.py
@@ -1,23 +1,28 @@
 # -*- coding: utf-8 -*-
 import logging
-from common.commonfunc import Login_func
+from common.commonfunc import Check_func
 import os
 from os import path
 import logging.config
 from config.appium_config import appium_desired
 
 
-class LoginView(Login_func):
+class LoginView(Check_func):
+
+
+    def loginin_not(self):
+
+        carno_weizhi=self.find_element('new UiSelector().className("android.widget.TextView").index(0)')
+        if carno_weizhi:
+            return False
+        else:
+            return True
 
     def login_action(self,loginname,password):
 
-        loginname_type = ('new UiSelector().text("请输入用户名")')
-
-        password_type = (
-        'new UiSelector().className("android.widget.EditText").resourceId("com.hs.mywork.activity:id/iev_pwd")')
-
-        loginin_type = (
-        'new UiSelector().className("android.widget.FrameLayout").resourceId("com.hs.mywork.activity:id/btn_login")')
+        loginname_type = 'new UiSelector().text("请输入用户名")'
+        password_type = 'new UiSelector().className("android.widget.EditText").resourceId("com.hs.mywork.activity:id/iev_pwd")'
+        loginin_type = 'new UiSelector().className("android.widget.FrameLayout").resourceId("com.hs.mywork.activity:id/btn_login")'
 
         tyename=self.check_element(loginname_type)
         tyepas=self.check_element(password_type)
@@ -30,12 +35,5 @@ class LoginView(Login_func):
         logging.info('login is worked  ')
 
 
-
-
-
-if __name__ == '__main__':
-    driver=appium_desired()
-    l=LoginView(driver)
-    l.login_action('027310','1')
 
 
--- moudle/index.py
+++ moudle/index.py
@@ -3,19 +3,24 @@
 import os
 from os import path
 import logging.config
-from config.appium_config import Appium_sedired
+from config.appium_config import *
 from bussinessView.loginView import LoginView
+from bussinessView.kuaisubaojiaView import JiecheView
 
-log_file_path = os.path.join(path.dirname(path.abspath(__file__)), 'log.config')
+log_file_path = os.path.join('..\config', 'log.config')
+# print(log_file_path)
 logging.config.fileConfig(log_file_path, disable_existing_loggers=False)
 logging=logging.getLogger()
 
 
 if __name__=='__main__':
-    appium=Appium_sedired()
-    appium.appium_desired()
-    logging.info('app start')
-    LoginView()
+    driver = appium_desired()
+    l=LoginView(driver)
+    if l.loginin_not is True:
+        print(l.loginin_not)
+        l.login_action('027310','1')
+    j=JiecheView(driver)
+    j.jieche_action('粤A88991')
 
 
 
